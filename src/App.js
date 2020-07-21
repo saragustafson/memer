aa17719d20161f489f3fdf4f453dc2a6ead128f7
saragustafson <saramgustafson@gmail.com>
2020-07-21 12:22:29 -0700

    memey

--- src/App.js
+++ src/App.js
@@ -1,27 +1,47 @@
-import React, {useState} from 'react';
-import './App.css';
+import React, {useState} from 'react'
+import './App.css'
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 
 function App() {
   const [text,setText] = useState('')
+  const [memes,setMemes] = useState([])
+
+  async function search(){
+    const key = 'bdHT5nMfUGwCdkfXJ8sJGOPui9uDqIwI'
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${key}&q=${text}&limit=25&offset=0&lang=en`
+    const r = await fetch(url)
+    const j = await r.json()
+    setMemes(j.data)
+    setText('')
+  }
+
   return (
     <div className="App">
-      <div className= "searchBar"> 
-        <TextField id="outlined-basic" label="Search" 
-          variant="outlined" color="Primary"
-          placeholder="Let's get memey" value={text}
-          onChange={e=> setText(e.target.value)} />
-
+      <div className="searchbar">
+        <TextField label="Search" variant="outlined" 
+          color="primary" placeholder="Let's get memey!"
+          value={text} onChange={e=> setText(e.target.value)}
+          onKeyPress={e=>{
+            if(e.key==='Enter') search()
+          }}
+        />
         <Button variant="contained" color="primary"
-        disabled={!text}>
-          Search
+          style={{height:55,marginLeft:8}}
+          disabled={!text} onClick={search}>
+          Go
         </Button>
-
       </div>
-
+      <div className="memes">
+        {memes.map((m,i)=>{
+          console.log(m)
+          return <img key={i} 
+            src={m.images.fixed_height.url}
+          />
+        })}
+      </div>
     </div>
   )
 }
 
-export default App
+export default App;
